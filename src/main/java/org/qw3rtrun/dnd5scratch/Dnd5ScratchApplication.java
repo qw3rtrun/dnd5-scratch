a7b93040d8404f5f9023d2cc02043f58e6c2a1d8
qw3rtrun <qw3rtrun@gmail.com>
2019-02-05 01:17:38 +0300

    Initial commit

---
+++ src/main/java/org/qw3rtrun/dnd5scratch/Dnd5ScratchApplication.java
@@ -0,0 +1,14 @@
+package org.qw3rtrun.dnd5scratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dnd5ScratchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dnd5ScratchApplication.class, args);
+    }
+
+}
+
